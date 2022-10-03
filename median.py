cb702429e6eb6d91bbc6620cf92a722a26ec8ac3
Ryan Lim Yi Ming (github acc) <k21001230@kcl.ac.uk>
2022-10-03 05:01:57 +0100

    Update median.py
    Add median funtion

--- median.py
+++ median.py
@@ -1,12 +1,20 @@
 """Median calculator."""
-"""ENTER YOUR SOLUTION HERE!"""
+import math
+def findMedian(numbers):
+    numbers.sort()
+    index = math.floor(len(numbers)/2)
+    if (len(numbers)/2)%1 == 0:
+        return (numbers[index-1] + numbers[index])/2
+    else:
+        return numbers[index]
 
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        print(findMedian(numbers))
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
